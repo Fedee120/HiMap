e89e96e11577c745364722af35ab2b5d04adc864
Fedee120 <federico.gil@fing.edu.uy>
2023-08-27 03:27:53 -0300

    corrected cors problem by deleting it and using nginx reverse proxy

--- backend/node-backend/server.js
+++ backend/node-backend/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const queryRoute = require('./routes/query');
-const cors = require('cors');
 
 const app = express();
 const PORT = 3001;
-
-app.use(cors()); // Middleware to enable CORS
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use('/api', queryRoute);
